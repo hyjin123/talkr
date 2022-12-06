69e0874fd4b5ebf56f62e2b4cb23c052246f0214
Sean <seanhoyeonjin@gmail.com>
2022-12-05 19:04:32 -0500

    users can now set their status on the settings page

--- components/SetStatusModal.js
+++ components/SetStatusModal.js
@@ -1,10 +1,8 @@
-import { View, Text, TouchableOpacity, TextInput, Alert } from "react-native";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 import React, { useState } from "react";
 import tw from "twrnc";
 import { auth, db } from "../firebase";
-import { useCollection } from "react-firebase-hooks/firestore";
 import Modal from "react-native-modal";
-import * as EmailValidator from "email-validator";
 
 const SetStatusModal = ({
   statusModalVisible,
@@ -12,28 +10,68 @@ const SetStatusModal = ({
   theme,
 }) => {
   const [status, setStatus] = useState("");
+  // status has a character limit of 40
+  const [count, setCount] = useState(40);
 
   // get the logged in user id through auth
-  const loggedInUserEmail = auth.currentUser.email;
+  const userid = auth.currentUser.uid;
+
+  // when user submits the status
+  const handleStatusChange = () => {
+    db.collection("users").doc(userid).set(
+      {
+        status,
+      },
+      { merge: true }
+    );
+
+    setStatusModalVisible(false);
+    setStatus("");
+    setCount(40);
+  };
 
   return (
     <Modal
       isVisible={statusModalVisible}
-      onBackdropPress={() => setStatusModalVisible(false)}
+      onBackdropPress={() => {
+        setStatusModalVisible(false);
+        setStatus("");
+        setCount(40);
+      }}
     >
-      <View
-        style={tw`flex-1 justify-center items-center bg-white my-80 mx-2 rounded-xl`}
-      >
-        <Text>Set your status</Text>
-        <TextInput
-          placeholder="What are you up to?"
-          placeholderTextColor="gray"
-          value={status}
-          onChangeText={(text) => setStatus(text)}
-          style={tw`border-2 border-gray-300 rounded-lg py-1 w-65 p-2 mt-3`}
-        />
+      <View style={tw`flex-1 items-center bg-white my-72 mx-2 rounded-xl`}>
+        <View style={tw`mt-8 mb-2`}>
+          <Text>Set your status</Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder="What are you up to?"
+            placeholderTextColor="gray"
+            value={status}
+            onChangeText={(text) => {
+              setStatus(text);
+              setCount(40 - text.length);
+            }}
+            style={tw`border-2 border-gray-300 rounded-lg py-1 w-65 p-2 mt-3`}
+          />
+        </View>
+
+        {/* Character limit */}
+        <View style={tw`flex-row self-start ml-11 mt-2`}>
+          <Text style={tw`${count > -1 ? "text-blue-500" : "text-red-500"}`}>
+            {count}{" "}
+          </Text>
+          <Text style={tw`text-gray-400`}>(characters remaining)</Text>
+        </View>
+
         <TouchableOpacity
-          style={tw`bg-[${theme?.primary[0]}] font-bold rounded-full px-15 py-2 mt-6`}
+          onPress={handleStatusChange}
+          disabled={count < 0 ? true : false}
+          style={tw`bg-[${
+            theme?.primary[0]
+          }] font-bold rounded-full px-15 py-2 mt-6 ${
+            count < 0 ? "bg-gray-300" : ""
+          }`}
         >
           <Text style={tw`text-black`}>Confirm</Text>
         </TouchableOpacity>
